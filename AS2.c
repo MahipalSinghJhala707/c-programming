94cbf632356b1833483cfd61619afe65d72b3b39
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-09 12:30:38 +0530

    AS2 complete

---
+++ AS2.c
@@ -0,0 +1,103 @@
+//Question 1
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+
+//#include<stdio.h>
+//int main(){
+//    int a;  //valid
+//    int 0a; //in valid
+//    int Zeronum; //valid 
+//    int _first; //valid
+//    int 009; // in valid
+//    int else; // in valid
+//}
+
+
+
+
+
+//Question 2
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+   int x;
+    x=10;
+    printf("Value of variable is %d",x);
+    return 0;
+}
+*/
+
+
+
+
+
+//Question 3
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x,y;
+    x=10;
+    y=20;
+    printf("Value of variable is %d\nValue of variable is %d",x,y);
+    return 0;
+}
+*/
+
+
+
+
+
+//Question 4
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    x=5;
+    y=6;
+    z=7;
+    printf("Value of variable is %d\nValue of variable is %d\nValue of variable is %d",x,y,z);
+    return 0;
+}
+*/
+
+
+
+
+
+
+//Question 5
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x,y,z'w;
+    x=15;
+    y=16;
+    z=17;
+    w=18;
+    printf("Value of first variable is %d\nValue of second variable is %d\nValue of third variable is %d\nValue of fourth variable is %d",x,y,z,w);
+    return 0;
+}
+*/
