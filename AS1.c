bfb15d3eef554a9d31a053bf3dc00e151184c2d1
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-09 11:40:45 +0530

    AS1 complete

---
+++ AS1.c
@@ -0,0 +1,13 @@
+/*Author; Mahipal singh jhala
+Date : 
+Aim : complete the assignment
+language : c programming
+*/
+#include<stdio.h>
+int main(){
+    printf("hello\n");
+    printf("hello world\n");
+    printf("Mahipal\nSingh\nJhala\nTechno NJR INDIA\n\"TECHNO NJR INDIA\"");
+    printf("HELLO\nWORLD");
+    return 0;
+}
