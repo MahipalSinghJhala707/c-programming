3f98543176f99faef2a7018c34bf34c0ab7ee168
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-09 15:26:45 +0530

    AS5 completed

---
+++ AS5.c
@@ -0,0 +1,154 @@
+/*Question 1
+#include<stdio.h>
+int main(){
+    float x,y,z,w;
+    printf("Enter the salary :");
+    scanf("%f",&x);
+    y=x*0.5;
+    z=x*0.2;
+    w=x+y+z;
+    printf("the gross salary is %f.",w);
+    return 0;
+}
+*/
+
+
+/*Question 2
+#include<stdio.h>
+int main(){
+    float x,y,z,;
+    printf("Enter the lenght :");
+    scanf("%f",&x);
+    printf("Enter the breath :");
+    scanf("%f",&y);
+    z=x*y;
+    printf("the area of rectangle is %f",z);
+    return 0;
+}
+*/
+
+
+/*Question 3
+#include<stdio.h>
+int main(){
+    float x,y,z,;
+    printf("Enter the lenght :");
+    scanf("%f",&x);
+    printf("Enter the breath :");
+    scanf("%f",&y);
+    z=2*(x+y);
+    printf("the peremeter of rectangle is %f",z);
+    return 0;
+}
+*/
+
+
+/*Question 4
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the side of square :");
+    scanf("%d",&x);
+    y=x*x;
+    printf("the area of square is %d",y);
+    return 0;
+}
+*/
+
+
+/*Question 5
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the side of square :");
+    scanf("%d",&x);
+    y=x*4;
+    printf("the peremeter of square is %d",y);
+    return 0;
+}
+*/
+
+
+/*Question 6
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the radius of circle :");
+    scanf("%d",&x);
+    y=x*x*3.14;
+    printf("the area of circle is %d",y);
+    return 0;
+}
+*/
+
+
+/*Question 7
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the radius of circle :");
+    scanf("%d",&x);
+    y=x*3.14*2;
+    printf("the area of circle is %d",y);
+    return 0;
+}
+*/
+
+
+/*Question 8
+#include<stdio.h>
+int main(){
+    int x,y,z,w;
+    printf("Enter the first side :");
+    scanf("%d",&x);
+    printf("Enter the second side :");
+    scanf("%d",&y);
+    printf("Enter the third side :");
+    scanf("%d",&z);
+    w=x+y+z;
+    printf("the peremeter of the triangle is %d",y);
+    return 0;
+}
+*/
+
+
+/*Question 9
+#include<stdio.h>
+int main(){
+    float x,y,z,w,v,u;
+    printf("Enter the marks of first subject :");
+    scanf("%f",&x);
+    printf("Enter the marks of second subject :");
+    scanf("%f",&y);
+    printf("Enter the marks of third subject :");
+    scanf("%f",&z);
+    printf("Enter the marks of fourth subject :");
+    scanf("%f",&w);
+    printf("Enter the marks of fifth subject :");
+    scanf("%f",&v);
+    u=x+y+z+w+v;
+    printf("the aggregate marks are %f",y);
+    return 0;
+}
+*/
+
+
+/*Question 10
+#include<stdio.h>
+int main(){
+    float x,y,z,w,v,u;
+    printf("Enter the marks of first subject :");
+    scanf("%f",&x);
+    printf("Enter the marks of second subject :");
+    scanf("%f",&y);
+    printf("Enter the marks of third subject :");
+    scanf("%f",&z);
+    printf("Enter the marks of fourth subject :");
+    scanf("%f",&w);
+    printf("Enter the marks of fifth subject :");
+    scanf("%f",&v);
+    u=(x+y+z+w+v)/5;
+    printf("the percentage marks are %f",y);
+    return 0;
+}
+*/
