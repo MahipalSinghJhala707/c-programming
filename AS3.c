6c1e2e8952ca15f6623bdb96f2c5963791826225
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-09 12:42:39 +0530

    AS3 completed

---
+++ AS3.c
@@ -0,0 +1,65 @@
+//Question 1
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter the value of variable :");
+    scanf("%d"&x);
+    printf(" the value of variable is %d",x);
+    return 0;
+}
+*/
+
+
+
+
+
+//Question 2
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("enter the value of first variable :");
+    scanf("%d"&x);
+    printf("enter the value of second variable :");
+    scanf("%d"&y);
+    printf("The value of first variable is %d\nThe value of second variable is %d",x,y);
+    return 0;
+}
+*/
+
+
+
+
+
+
+//Question 3
+/*Author; Mahipal singh jhala
+Date : 27/09/22
+Aim : complete the assignment
+language : c programming
+*/
+/*
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    printf("enter the value of first variable :");
+    scanf("%d"&x);
+    printf("enter the value of second variable :");
+    scanf("%d"&y);
+    printf("enter the value of third variable :");
+    scanf("%d"&z);
+    printf("The value of first variable is %d\nThe value of second variable is %d\nThe value of second variable is %d",x,y,z);
+    return 0;
+}
+*/
