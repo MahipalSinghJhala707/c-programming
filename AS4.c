063c6a8879e00bc7e3b62a246d1f3eec163003cc
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-09 13:20:35 +0530

    AS4 completed

---
+++ AS4.c
@@ -0,0 +1,130 @@
+/*Question 1
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("enter two integers to add :");
+    scanf("%d%d"&x,&y);
+    sum=x+y;
+    printf("the sum of two integers is %d",sum);
+    return 0;
+}
+*/
+
+
+/*Question 2
+#include<stdio.h>
+int main(){
+    int x,y,sub;
+    printf("enter two integers to subtract :");
+    scanf("%d%d"&x,&y);
+    sub=x-y;
+    printf("the difference between two integers is %d",sub);
+    return 0;
+}
+*/
+
+
+/*Question 3
+#include<stdio.h>
+int main(){
+    int x,y,p;
+    printf("enter two integers to multiply :");
+    scanf("%d%d"&x,&y);
+    p=x*y;
+    printf("the product of two integers is %d",p);
+    return 0;
+}
+*/
+
+
+/*Question 4
+#include<stdio.h>
+int main(){
+    int x,y,q;
+    printf("enter two integers to divide :");
+    scanf("%d%d"&x,&y);
+    q=x/y;
+    printf("the quotient is %d",q);
+    return 0;
+}
+*/
+
+
+/*Question 5
+#include<stdio.h>
+int main(){
+    float x,y,z;
+    printf("Enter the basic salary :");
+    scanf("%f",x);
+    y=x*0.4;
+    z=x+y;
+    printf("the gross salary is %f",z);
+    return 0;
+}
+*/
+
+
+/*Question 6
+#include<stdio.h>
+int main(){
+    float x,y;
+    printf("Enter the distance :");
+    scanf("%f",x);
+    y=x*1000;
+    printf("the distance in metres is %f",y);
+    return 0;
+}
+*/
+
+
+/*Question 7
+#include<stdio.h>
+int main(){
+    float x,y;
+    printf("Enter the distance :");
+    scanf("%f",x);
+    y=x*100000;
+    printf("the distance in centimetres is %f",y);
+    return 0;
+}
+*/
+
+
+/*Question 8
+#include<stdio.h>
+int main(){
+    float x,y;
+    printf("Enter the distance :");
+    scanf("%f",x);
+    y=x*1000000;
+    printf("the distance in milimetres is %f",y);
+    return 0;
+}
+*/
+
+
+/*Question 9
+#include<stdio.h>
+int main(){
+    float x,y;
+    printf("Enter the height in feet :");
+    scanf("%f",x);
+    y=x*12;
+    printf("the height in inches is %f",y);
+    return 0;
+}
+*/
+
+
+/*Question 10
+#include<stdio.h>
+int main(){
+    float x,y;
+    printf("Enter the temperature in fahrenheit :");
+    scanf("%f",&x);
+    y=(x-32)*5/9;
+    printf("The temperature in celsius is %f .",y);
+    return 0;
+
+}
+*/
