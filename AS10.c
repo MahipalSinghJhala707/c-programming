b1714694aa57bce0242f54f1f1b0d074512bd962
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-11 21:07:10 +0530

    AS10-11 complete

---
+++ AS10.c
@@ -0,0 +1,188 @@
+/*Question 1
+#include<stdio.h>
+int main() {
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 5/2;
+f= 5/2;
+printf("Value of integer i is %d\n", i);
+printf("Value of float f is %f\n",f);
+return 0;
+}
+*/
+
+
+/*Question 2
+#include<stdio.h>
+int main()
+{
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 5.0/2;
+f= 5.0/2;
+printf("Value of integer i is %d\n",i);
+printf("Value of float f is %f\n",f);
+return 0;
+}
+*/
+
+
+/*Question 3
+#include<stdio.h>
+int main()
+{
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 5/2.0;
+f= 5/2.0;
+printf("Value of integer i is %d\n",i); 
+printf("Value of float f is %f\n",f);
+ return 0;
+
+}
+*/
+
+
+/*Question 4
+#include<stdio.h>
+int main()
+{
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 5.0/2.0;
+f= 5.0/2.0;
+printf("Value of integer i is %d\n",i);
+printf("Value of float f is %f\n",f);
+return 0;
+
+}
+*/
+
+
+/*Question 5
+#include<stdio.h>
+int main()
+{
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 2/5;
+f= 2/5;
+printf("Value of integer i is %d\n",i); 
+printf("Value of float f is %f\n",f);
+return 0;
+}
+*/
+
+
+/*Question 6
+#include<stdio.h>
+int main(){
+    int i;
+    float f;
+    printf("Testing of various arthmetic opreators\n");
+    i = 2.0/5;
+    f = 2.0/5;
+    printf("value of integer i is %d\n",i);
+    printf("value of float f is %f \n",f);
+    return 0;
+}
+*/
+
+
+/*Question 7
+#include<stdio.h>
+int main(){
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 5/2;
+f= 5/2;
+printf("Value of integer i is %d\n", i);
+printf("Value of float f is %f\n",f);
+return 0;
+}
+*/
+
+
+/*Question 8
+#include<stdio.h>
+int main(){
+int i;
+float f;
+printf("Testing of various arithmetic operations\n");
+i = 2/5 ;
+f = 2/5;
+printf("Value of integer i is %d\n",i); printf("Value of float f is %f\n",f); return 0;
+}
+*/
+
+
+/*Question 9
+#include<stdio.h>
+int main(){
+int i;
+printf("Testing of various arithmetic operations-2\n");
+i = 2/9;
+printf("1. Value of integer i is %d\n",i);
+i = 2.0/9;
+printf("2. Value of integer i is %d\n",i);
+i = 2/9.0;
+printf("3. Value of integer i is %d\n",i);
+i = 2.0/9.0;
+printf("4. Value of integer i is %d\n",i);
+i = 9/2;
+printf("5. Value of integer i is %d\n",i);
+i = 9.0/2;
+printf("6. Value of integer i is %d\n", i);
+i = 9/2.0;
+printf("7. Value of integer i is %d\n",i);
+i = 9.0/2.0; printf("8. Value of integer i is %d\n",i);
+return 0;
+}
+*/
+
+
+/*Question 10
+#include<stdio.h>
+int main(){
+float f;
+printf("Testing of various arithmetic operations-2\n");
+f = 2/9;
+printf("1. Value of float f is %f\n",f);
+f = 2.0/9; printf("2. Value of float f is %f\n",f);
+f = 2/9.0;
+printf("3. Value of float f is %f\n",f);
+f = 2.0/9.0;
+printf("4. Value of float f is %f\n",f);
+f = 9/2;
+printf("5. Value of float f is %f\n",f);
+f = 9.0/2;
+printf("6. Value of float f is %f\n",f);
+f = 9/2.0;
+printf("7. Value of float f is %f\n",f);
+f = 9.0/2.0; printf("8. Value of float f is %f\n",f);
+return 0;
+}
+*/
+
+
+/*Question 11
+#include<stdio.h>
+// Convert Fahrenheit temperature to Celcius equivalents
+int main(){
+int fahr, celcius;
+printf("Enter temperature in Fahr\n");
+scanf("%d",&fahr);
+celcius = 5/9 * (fahr-32);
+//Uncomment the following and test your output (comment the previous celcius calculation)
+//celcius = 5 + (fahr-32) / 9;
+//celcius 5.0/9 (fahr-32);
+printf("Temperature in Celcius is = %d\n", celcius);
+return 0;
+}
+*/
