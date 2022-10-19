2ad26ea7b1b5900f7b2df516ce4f805d9e017447
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2022-10-19 21:02:17 +0530

    done

--- fabonacci.c
+++ fabonacci.c
@@ -1,28 +1,18 @@
 #include<stdio.h>
 int main(){
-    int x,y,z,w,u;
-    printf("how long do you need fibonacci series to be");
-    scanf("%d",&z);
-    x=0;
+    int x,y,z,w,v,u;
+    z=0;
     y=1;
-    w=0;
-    if (z==1){
-        printf("%d ",x);
-    }
-    else if (z==2){
-        printf("%d %d",x,x+1);
-        
-    }
+    v=0;
+    u=0;
+    printf("enter the no. of terms you want of fibonacci series :");
+    scanf("%d",&x);
+    while(z<=x){
+    printf("%d \n", z);
+    w=y+z;
+    z=y;
+    y=w;
     
-    else {
-        printf("%d %d ",x,x+1);
-        while(w<=z-3){
-            u=x+y;
-            printf("%d ",u);
-            u=x+y;
-            x=y;
-            y=u;
-            w+=1;
-        }
-    }
+}
+    return 0;
 }
