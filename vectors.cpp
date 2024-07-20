e73af70f03b12d4be034c801190a9f6cf90713a2
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-07-20 10:06:17 +0530

    -

---
+++ vectors.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // vector<int> v ;
+    // v.push_back(1);
+    // v.emplace_back(2);
+    // v.push_back(3);
+    // cout<<v[0];
+    // cout<<v[1];
+    // cout<<v[2];
+    // vector<pair<int, int>> p;
+    // p.push_back({1,3});
+    // p.emplace_back(2,5);
+    // cout<<p[0].first;
+    // cout<<p[0].second;
+    // cout<<p[1].first;
+    // cout<<p[1].second<<endl;
+    // vector<int> x(5,100);
+    // vector<int> x2(5);
+    // vector<int> x3(x);
+    // cout<<x[0]<<" "<<x[1]<<endl;
+    // cout<<x2[0]<<" "<<x2[1]<<endl;
+    // cout<<x3[0]<<" "<<x3[1]<<endl;
+
+
+
+    vector<int> y = {10,20,30,40,50};
+
+    vector<int>::iterator i = y.begin();
+    cout<<*i<<"\t";
+    i++;
+    cout<<*i<<"\t";
+    i = y.end();//points to the memory next to the last element
+    cout<<*i<<"\t";
+    i--;
+    cout<<*i<<"\t";
+    vector<int>::reverse_iterator j = y.rbegin();
+    cout<<*j<<"\t";
+    j++;
+    cout<<*j<<"\t";
+    j=y.rend();
+    cout<<*j<<"\t";
+    j--;
+    cout<<*j<<"\t";
+
+    for (vector<int>::iterator i =y.begin(); i != y.end(); i++)
+    {
+        cout<<*i<<",";
+    }
+    cout<<endl;
+    for (auto i =y.begin(); i != y.end(); i++)
+    {
+        cout<<*i<<"-";
+    }
+
+    cout<<endl;
+    for(auto it : y)
+        cout<<it<<" ";
+
+    
+    return 0;
+}
